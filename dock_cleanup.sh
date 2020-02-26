46f212bf5ee70c59146cb7cf10564ca11bb0ca09
Dustin J. Landgraf <dustinjlandgraf@gmail.com>
2020-02-26 11:33:53 -0600

    Adding status commands for depnotify

--- dock_cleanup.sh
+++ dock_cleanup.sh
@@ -2,7 +2,7 @@
 
 #This script is intended to get rid of some of the default nonsense in the Dock when a new user logs in for the first time
 #DJL Feb 19, 2020
-
+echo "Status: Cleaning up the Dock" >> /var/tmp/depnotify.log
 /usr/local/bin/dockutil --remove 'Mail'
 /usr/local/bin/dockutil --remove 'Calendar'
 /usr/local/bin/dockutil --remove 'Contacts'
@@ -12,5 +12,5 @@
 /usr/local/bin/dockutil --remove 'Music'
 /usr/local/bin/dockutil --remove 'TV'
 /usr/local/bin/dockutil --remove 'Podcasts'
-
+echo "Status: " >> /var/tmp/depnotify.log
 exit 0
